a0a59716497db92f5cf1b20eb447fe5b78913232
GodStar88 <godstar0808@gmail.com>
2021-12-03 11:27:08 +0800

    update page

--- index.php
+++ index.php
@@ -20,12 +20,9 @@
             display: none;
         }
 
-        .title {
-            font-size: 25px;
+        .fontcolor {
             color: rgb(224, 128, 28);
             text-align: center;
-            margin-top: 70px;
-            line-height: 50px;
             font-weight: bold;
         }
 
@@ -80,16 +77,79 @@
             <button type="button" class="btn btn-rounded btn-info" style="float:right" onclick="location.href='login';">Login</button>
             <div class="container">
                 <div class="row">
-                    <h2 class="title">Freemailer is a revolutionary lead contact software that allows you to contact the exact leads you need, bypass the gatekeepers of companies, and land clients effortlessly without using email at all.</h2>
-                    <div class="line" style="width:50%; margin-top: 30px;"></div>
+                    <h1 class="fontcolor" style="margin: auto; margin-top: 50px;">Making Lead Magnets that Convert is HARD!</h1>
+                </div>
+                <div class="row">
+                    <img src="assets/images/face.jpg" alt="" width="200px" height="200px" style="margin: auto; margin-top: 50px;">
+                </div>
+                <div class="row">
+                    <h2 class="fontcolor" style="margin: auto; margin-top: 50px;">All the Time and Trouble to get a good lead magnet that will convert for your offer can drive you CRAZY!</h2>
+                </div>
+                <div class="row">
+                    <h3 class="fontcolor" style="margin: auto; margin-top: 50px;">Not long ago I had the same issue. I had a great product and I knew if I could just build a lead magnet that would attract people to sign up to my list and introduce them to what I had to offer I could convert them to buy.</h3>
+                </div>
+                <div class="row">
+                    <img src="assets/images/people.jpg" alt="" width="400px" height="200px" style="margin: auto; margin-top: 50px;">
+                </div>
+                <div class="row">
+                    <h3 class="fontcolor" style="margin: auto; margin-top: 50px;">I also wanted them to get lots of value and have a great experience to provide that “know-like-trust” factor so I would be the “Good Guy” they would trust and buy from again and again. I tried different ebooks and videos and educational courses and, and, and ….   my conversions were still poor :( </h3>
+                </div>
+                <div class="row">
+                    <h1 class="fontcolor" style="margin: auto; margin-top: 50px;">But then I realized something!</h1>
+                </div>
+                <div class="row">
+                    <img src="assets/images/realized.jpg" alt="" width="200px" height="280px" style="margin: auto; margin-top: 50px;">
+                </div>
+                <div class="row">
+                    <h3 class="fontcolor" style="margin: auto; margin-top: 50px;">In order to REALLY build a Lead Magnent that could deliver what I needed I would need to go beyond what other people usually provide. </br></br>
+                    I would need to STAND OUT and set myself apart from the common offers that are used as Lead Magnets.</br></br>
+                    My idea was to build a full blown system that was SO outstanding it would be a Super No Brainer for my prospects to sign up. Something everyone would want!</br></br>
+                    This was the start of ULTIMATE LEAD MAGNETS!</br></br>
+                    I built the first Ultimate Lead Magnet to prove the concept and sent traffic to it on a Tuesday morning. I tracked the click to sign up ratio and WOW!</br></br>
+                    I knew right then I was on to something :)</br></br>
+                    Previosly, my best conversion rate had been 23% from a highly desirable course I had gotten the resell rights to.</br></br>
+                    But that Tuesday, I got a conversion rate of 77%!!!</br></br>
+                    I had never seen conversions like this. I knew I was on to something big!</br></br>
+                    SO, I gave the Ultimate Lead Magnet to a few friends of mine in the Affiliate Marketing space and WHAM, they got the same results!
+                    </h3>
+                </div>
+                <div class="row">
+                    <h2 class="fontcolor" style="margin: auto; margin-top: 50px;">That was the day I knew I had to pursue this Ultimate Lead Magnet system.</h2>
+                </div>
+                <div class="row">
+                    <h3 class="fontcolor" style="margin: auto; margin-top: 50px;">
+                        Next, I built and tested another one and then another one.</br></br>
+                        I got a group of Beta testers involved and soon my dream of being able to provide this system to others was realized.</br></br>
+                        Now, the Ultimate Lead Magnet system is ready for the marketing community to use.</br></br>
+                        So what IS the Ultimate Lead Magnet system?</br></br>
+                        Well, the Ultimate Lead Magnet system is a series of 3 special web sites that someone would normally pay money to access.</br></br>
+                        The person who wants to give away access to these highly desirable web sites receives a special code that  allows them to set up a referral link and then give the web  site access away. When their prospects sign up the  name and email of those prospects is collected in their account and they can download the entire file of sign ups anytime they choose.</br></br>
+                        Simply give away the link to the web site and collect your signups on demand!</br></br>
+                        These web sites are PROVEN to convert!</br></br>
+                        You will never need to build a Lead Magnet again!</br></br>
+                    </h3>
+                </div>
+                <div class="row">
+                    <h2 class="fontcolor" style="margin-top: 50px;">Step one: Sign up to Ultimate Lead Magnets</h2>
+                    <h2 class="fontcolor">Step Two: Sign up to the Web sites and get your referral link</h2>
+                    <h2 class="fontcolor">Step Three: Give away your referral link and collect your optins</h2>
                 </div>
 
-                <div class="row" style="margin-top: 30px;">
-                    <div class="col-md-12 video-container">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/qbS3utktitw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
+                <div class="row">
+                    <h1 style="margin-top: 50px;color:white">Q & A</h1>
+                  
                 </div>
-
+                <div class="row">
+                    <h3 style="color:white">Question: How do I tell prospects about these web sites?</h3>
+                    <h3 style="color:white">Answer: You can post your referral link anywhere you normally contact prospects. Advertise it. Put it on your web site. Send it to Social contacts. Let everyone know you think is a good target for your marketing! </h3>
+                    <h3 style="color:white">Question: How long does it take to set this up?</h3>
+                    <h3 style="color:white">Answer: About 5 minutes, one time</h3>
+                    <h3 style="color:white">Question: What kind of Web Sites would I be giving away?</h3>
+                    <h3 style="color:white">Answer: These are Web sites that anyone in the Internet Marketing, Make Money online, Affiliate Marketing, MLM/Netowrk Marketing, Digital Marketing, and Local Business Owners would LOVE to have access to, especially for FREE!</h3>
+                    <h3 style="color:white">How long is this offer good for?</h3>
+                    <h3 style="color:white">Answer: Currently, space is limited, especially at the launch price. We will be going to a monthly recurring price after launch. </br></br> Testimonials </br></br> Buy Button</br>Intro price $9 or $11</h3>
+                </div>
+                
                 <div class="text-center">
                     <br>
                     <br>
